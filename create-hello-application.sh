b8757a825902f84f90c848504410b53149c82043
Jim Lin <jimlintw922@gmail.com>
2017-08-27 20:12:14 +0800

    add hello application

--- create-hello-application.sh
+++ create-hello-application.sh
@@ -3,8 +3,8 @@
 echo "Y" | mvn archetype:generate -DarchetypeGroupId=org.opendaylight.controller -DarchetypeArtifactId=opendaylight-startup-archetype \
 -DarchetypeRepository=http://nexus.opendaylight.org/content/repositories/opendaylight.release/ \
 -DarchetypeCatalog=remote -DarchetypeVersion=1.2.3-Boron-SR3 \
--DgroupId=org.opendaylight.example \
--DartifactId=example \
--Dpackage=org.opendaylight.example \
--DclassPrefix=Example \
+-DgroupId=org.opendaylight.hello \
+-DartifactId=hello \
+-Dpackage=org.opendaylight.hello \
+-DclassPrefix=Hello \
 -Dcopyright="CC, Inc."
