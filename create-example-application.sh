0ad732536dc484d346a3486df44ba5757aa4d08f
Jim Lin <jimlintw922@gmail.com>
2017-08-27 20:02:42 +0800

    add create-example-application.sh

--- create-example-application.sh
+++ create-example-application.sh
@@ -1,15 +1,5 @@
-# For Boron-SR3
+#!/bin/bash
 
-# Setup development environment
-
-```
-wget https://raw.githubusercontent.com/opendaylight/odlparent/stable/boron/settings.xml -O ~/.m2/settings.xml
-```
-
-# Create a new application
-
-```
 mvn archetype:generate -DarchetypeGroupId=org.opendaylight.controller -DarchetypeArtifactId=opendaylight-startup-archetype \
 -DarchetypeRepository=http://nexus.opendaylight.org/content/repositories/opendaylight.release/ \
 -DarchetypeCatalog=remote -DarchetypeVersion=1.2.3-Boron-SR3
-```
