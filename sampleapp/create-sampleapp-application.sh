ece42dee8c25195754d837623fa4f650f4992c32
Jim Lin <jimlintw922@gmail.com>
2017-08-29 15:30:05 +0800

    update create-sampleapp-application.sh

--- sampleapp/create-sampleapp-application.sh
+++ sampleapp/create-sampleapp-application.sh
@@ -23,3 +23,30 @@ echo "Y" | mvn archetype:generate \
 # * impl/pom.xml
 # * impl/src/main/resources/org/opendaylight/blueprint/impl-blueprint.xml
 # * features/src/main/features/features.xml
+
+<<CMD
+cd sampleapp
+mvn clean install
+
+cp to-be/sampleapp-1/sampleapp.yang api/src/main/yang/sampleapp.yang
+cp to-be/sampleapp-1/api/pom.xml api/pom.xml
+cd api
+mvn clean install
+cd ..
+
+cp to-be/sampleapp-2/SampleAppListener.java impl/src/main/java/org/opendaylight/sampleapp/impl/SampleAppListener.java
+cd to-be/sampleapp-2/impl-blueprint.xml impl/src/main/resources/org/opendaylight/blueprint/impl-blueprint.xml
+cd impl
+mvn clean install
+cd ..
+
+cp to-be/sampleapp-3/*.java impl/src/main/java/org/opendaylight/sampleapp/impl/
+cp to-be/sampleapp-3/impl/pom.xml impl/pom.xml
+cp to-be/sampleapp-3/impl-blueprint.xml impl/src/main/resources/org/opendaylight/blueprint/impl-blueprint.xml
+cp to-be/sampleapp-3/features.xml features/src/main/features/features.xml
+cd impl
+mvn clean install
+cd ..
+
+mvn clean install
+CMD
