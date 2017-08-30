41db1dc575c2a3bab5705df3829f3f8ad334a579
Jim Lin <jimlintw922@gmail.com>
2017-08-30 09:43:13 +0800

    update create-sampleapp-application.sh

--- sampleapp/create-sampleapp-application.sh
+++ sampleapp/create-sampleapp-application.sh
@@ -31,31 +31,31 @@ echo "Y" | mvn archetype:generate \
 
 <<CMD
 cd sampleapp
-mvn clean install
+mvn clean install -DskipTests
 
 cp to-be/sampleapp-1/sampleapp.yang api/src/main/yang/sampleapp.yang
 cp to-be/sampleapp-1/api/pom.xml api/pom.xml
 cd api
-mvn clean install
+mvn clean install -DskipTests
 cd ..
 
 cp to-be/sampleapp-2/SampleAppListener.java impl/src/main/java/org/opendaylight/sampleapp/impl/SampleAppListener.java
 cp to-be/sampleapp-2/impl-blueprint.xml impl/src/main/resources/org/opendaylight/blueprint/impl-blueprint.xml
 cd impl
-mvn clean install
+mvn clean install -DskipTests
 cd ..
 
 cp to-be/sampleapp-3/*.java impl/src/main/java/org/opendaylight/sampleapp/impl/
 cp to-be/sampleapp-3/impl-blueprint.xml impl/src/main/resources/org/opendaylight/blueprint/impl-blueprint.xml
 cp to-be/sampleapp-3/impl/pom.xml impl/pom.xml
 cd impl
-mvn clean install
+mvn clean install -DskipTests
 cd ..
 cp to-be/sampleapp-3/features.xml features/src/main/features/features.xml
 cp to-be/sampleapp-3/features/pom.xml features/pom.xml
 cd features
-mvn clean install
+mvn clean install -DskipTests
 cd ..
 
-mvn clean install
+mvn clean install -DskipTests
 CMD
